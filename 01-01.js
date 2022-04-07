4881bfbb69596b6fb6406a5d3fde2728065f6bda
Aliaksandr-Batskalevich <aliaksandr.batskalevich@gmail.com>
2022-04-07 13:07:10 +0300

    Initial commit

---
+++ 01-01.js
@@ -0,0 +1,28 @@
+const object = {
+    name: 'ALex',
+    age: 33,
+    hasWife: true,
+    friends: [
+        {name: 'Andrej', age: 26},
+        {name: 'Olga', age: 25}
+    ]
+};
+
+
+const newObject = object;
+
+newObject.friends[0].name = 'Petr';
+console.log(object.friends[0].name);
+
+
+const spreadCopyObject = {...object}
+
+
+const hardCopyObject = {
+    ...object,
+    friends: object.friends.map(friend => ({...friend}))
+}
+
+hardCopyObject.friends[0].name = 'Nick';
+console.log(object.friends[0].name);
+console.log(hardCopyObject.friends[0].name);
